99197376aa247df95e8ff49ead55671b78e37f5d
unknown <arunsingh8775@gmail.com>
2025-08-24 16:42:06 +0530

    Adding InventoryStatus and InventoryStatus Model

---
+++ src/main/java/com/example/ordertrackingcommon/model/InventoryEvent.java
@@ -0,0 +1,16 @@
+package com.example.ordertrackingcommon.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryEvent {
+    private String eventType;     // "InventoryEvent"
+    private Long orderId;
+    private String productId;
+    private Integer qty;
+    private InventoryStatus status;
+}
