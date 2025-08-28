07a02c2286292593b1682110df0bf37d432f6e32
unknown <arunsingh8775@gmail.com>
2025-08-28 08:40:53 +0530

    Mark InventoryEntity as JPA entity

--- src/main/java/com/example/orderplatformcommon/model/InventoryEntity.java
+++ src/main/java/com/example/orderplatformcommon/model/InventoryEntity.java
@@ -1,18 +1,31 @@
 package com.example.orderplatformcommon.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "inventory")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class InventoryEntity {
+
+    @Id
+    @Column(name = "product_id")
     @JsonAlias({"productId","product_id"})
     private String productId;
 
+    @Column(name = "product_name")
     @JsonAlias({"productName","product_name"})
     private String productName;
 
+    @Column(name = "available_qty")
     @JsonAlias({"availableQty","available_qty"})
     private Integer availableQty;
 
-    private java.math.BigDecimal price;
+    @Column(name = "price", precision = 19, scale = 2)
+    private BigDecimal price;
 }
